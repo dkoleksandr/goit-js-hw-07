12ae07aaee6a556628c3b287054c194a3598b0ef
dkoleksandr <didenk.oleksandr@gmail.com>
2024-03-17 22:07:20 +0200

    add 4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const formEL = document.querySelector('.login-form');
+const emailEl = document.querySelector('.login-form input[type="email"]');
+const pwdEl = document.querySelector('.login-form input[type="password"]');
 
+formEL.addEventListener('submit', handlerForm);
+
+function handlerForm(event) {
+  event.preventDefault();
+  
+  const email = formEL.elements.email;
+  const password = formEL.elements.password;
+
+  if (email.value === '' || password.value === '') {
+    alert('All form fields must be filled in');
+  }
+  console.log({
+    [email.type]: email.value.trim(),
+    [password.type]: password.value.trim(),
+  });
+
+    formEL.reset();
+}
