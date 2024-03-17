8b5bca5278e1acfc82ffb9c16b3e5d1087880a07
dkoleksandr <didenk.oleksandr@gmail.com>
2024-03-17 22:18:23 +0200

    add 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnEL = document.querySelector('.change-color');
+const colorEL = document.querySelector('.color');
+
+btnEL.addEventListener('click', handleChangeColor);
+
+function handleChangeColor(event) {
+  colorEL.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorEL.textContent;
+}
